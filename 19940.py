d4a4b777617f6138c6129c69fb983c4ca1ac6bd2
Yeojun Lim <yeojun.lim@kakao.com>
2025-05-27 18:49:29 +0900

    19940 미해결

--- 19940.py
+++ 19940.py
@@ -8,36 +8,36 @@ def solve(time):
     if t > 3:
         t -= 6
         h += 1
-    
+
     if o > 5:
         o -= 10
         t += 1
-    
-    if t < o and o == 5:
-        t += 1
-        o -= 10
-    
+
     actions = [0]*5
 
     # h는 무조건 양수 또는 0이므로 그냥 더하기
     actions[0] += h
-    
+
     if t >= 0:
         actions[1] += t
     else:
         actions[2] += abs(t)
-    
+
     if o >= 0:
         actions[3] += o
     else:
         actions[4] += abs(o)
-    
+
     return actions
 
 
 
 T = int(sys.stdin.readline())
+results = []
 
 for _ in range(T):
     N = int(sys.stdin.readline())
-    print(*solve(N))
+    results.append(solve(N))
+
+for i in range(T):
+    print(*results[i])
